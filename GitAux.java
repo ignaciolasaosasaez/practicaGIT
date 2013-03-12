a23d50bf02e88a5aec52943acf88f23a6788566a
Juan Manuel Ruiz Gil <juanmanuelruizgil@uma.es>
2013-03-12 12:14:33 +0100

    GitAux Modificado

--- GitAux.java
+++ GitAux.java
@@ -1,11 +1,10 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ Liena de prueba
  */
 package prgitejemplo;
 
 /**
- *
+	Alumno: JUAN MANUEL RUIZ GIL
  * @author bertoa
  */
 public class GitAux {
