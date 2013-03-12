97ddec916a3b17aba7861f2905ebc6744874cf1d
Juan Manuel Ruiz Gil <juanmanuelruizgil@uma.es>
2013-03-12 12:20:14 +0100

    GitAux en rama2

--- GitAux.java
+++ GitAux.java
@@ -20,4 +20,8 @@ public class GitAux {
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
     }
+public void testGitAux(){
+// metodo rama testing
+System.out.println("Metodo de testing");
+}
 }
